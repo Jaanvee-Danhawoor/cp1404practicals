24688c9c99e139d08a2c5d44e7fa4371026a85ad
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-09-28 19:15:42 +1000

    Add module docstrings and function docstrings to password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,13 +1,19 @@
+"""Password check program"""
+
+
 def main():
+    """Get valid password and print asterisks as long as the password"""
     password = get_password()
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """Print asterisks as long as the password"""
     print("*" * len(password))
 
 
 def get_password():
+    """Get password of length greater than or equal to minimum_length"""
     minimum_length = 5
     password = input("Password: ")
     while len(password) < minimum_length:

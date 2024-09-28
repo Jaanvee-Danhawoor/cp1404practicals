85dc4e9916821ae8261e397eba6a357a6df7ccb3
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-09-28 19:04:54 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,18 @@
-minimum_length = 5
-password = input("Password: ")
-while len(password) < minimum_length:
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
+    minimum_length = 5
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < minimum_length:
+        password = input("Password: ")
+    return password
+
+
+main()

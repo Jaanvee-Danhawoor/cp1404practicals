930df44c8bcbacb1f4b3c49a8b38626ded11acb5
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-09-28 20:35:56 +1000

    Add print_result() function to score menu program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,13 +2,14 @@ MENU = "(G)et a valid score\n(P)rint result\n(S)how stars \n(Q)uit"
 
 
 def main():
+    score = -1
     print(f"Menu:\n{MENU}")
     choice = input("Choose: ").upper()
     while choice != "Q":
         if choice == "G":
             score = get_valid_score()
         elif choice == "P":
-            pass
+            print_result(score)
         elif choice == "S":
             pass
         else:
@@ -24,6 +25,17 @@ def get_valid_score():
         print("Invalid score")
         score = int(input("Score: "))
     return score
+
+
+def print_result(score):
+    if score < 0 or score > 100:
+        print("Invalid Score")
+    elif score < 50:
+        print("Bad")
+    elif score < 90:
+        print("Passable")
+    else:
+        print("Excellent")
 
 
 main()

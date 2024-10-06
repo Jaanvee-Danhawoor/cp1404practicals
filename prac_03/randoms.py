4364fc3a6cd24fc6de34f060e8b71b986550457b
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-10-06 19:38:05 +1000

    Generate random number between 1 and 100 inclusive

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -10,6 +10,9 @@
 # Could line 2 have produced a 4?
 # No, the step of 2 only produces odd numbers (3, 5, 7, 9).
 # What did you see on line 3?
-# The print statement was generating a random floating-point number between 2.5 and 5.5 inclusive.
+# The print statement was generating a random floating-point number between 2.5 and 5.5.
 # What was the smallest number you could have seen, what was the largest?
-# 2.5 was the smallest and 5.5 was the largest
+# 2.5471868652035234 was the smallest and 5.455893537149981 was the largest
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive
+import random
+print(random.randint(1, 100))

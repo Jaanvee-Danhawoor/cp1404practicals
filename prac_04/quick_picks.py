160b3a411ca83d1a4fe0f86f8d73ca9373dce581
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-10-13 20:35:35 +1000

    Refactor main() function to add generate_quick_pick() function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,13 +8,18 @@ NUMBER_PER_QUICK_PICK = 6
 def main():
     number_of_quick_picks = int(input("How many quick picks? "))
     for i in range(number_of_quick_picks):
-        numbers = []
-        while len(numbers) < NUMBER_PER_QUICK_PICK:
-            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
-            if number not in numbers:
-                numbers.append(number)
-            numbers = sorted(numbers)
+        numbers = generate_quick_pick()
         print(" ".join(f"{number:2d}" for number in numbers))
+
+
+def generate_quick_pick():
+    numbers = []
+    while len(numbers) < NUMBER_PER_QUICK_PICK:
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        if number not in numbers:
+            numbers.append(number)
+        numbers = sorted(numbers)
+    return numbers
 
 
 main()

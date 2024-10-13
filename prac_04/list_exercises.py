13dfd8cbb50feb1cc43f3d6d2aebb5d169239f37
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-10-13 19:50:37 +1000

    Add username authentication check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -14,3 +14,12 @@ print(f"The last number is {last_number}")
 print(f"The smallest number is {smallest_number}")
 print(f"The largest number is {largest_number}")
 print(f"The average of the numbers is {average_number}")
+
+# 2. Woefully inadequate security checker...
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter username:")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")

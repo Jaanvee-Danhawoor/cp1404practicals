191eeec0464cb7bcaed42d45898d58246a1bc2c1
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-10-20 18:51:47 +1000

    Complete prac 5 word occurrences exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,8 +1,7 @@
 COLOUR_TO_CODE = {"absolutezero": "#0048ba", "acid green": "#b0bf1a", "aliceblue": "#f0f8ff",
-                "alizarincrimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00",
-                "amethyst": "#9966cc", "antiquewhite": "#faebd7", "antiquewhite1": "#ffefdb",
-                "apricot": "#fbceb1"}
-
+                  "alizarincrimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00",
+                  "amethyst": "#9966cc", "antiquewhite": "#faebd7", "antiquewhite1": "#ffefdb",
+                  "apricot": "#fbceb1"}
 
 colour = input("Enter colour name: ").lower()
 while colour != "":

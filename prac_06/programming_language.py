470a51a1bfe43432cb36cbdede3fe4479f74acfb
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-10-31 15:07:18 +1000

    Print the names of all the languages with dynamic typing

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,16 +4,16 @@
 class ProgrammingLanguage:
     """Represent a ProgrammingLanguage object."""
 
-    def __init__(self, field="", typing="", reflection=False, year=0):
+    def __init__(self, name="", typing="", reflection=False, year=0):
         """Initialise a ProgrammingLanguage instance."""
-        self.field = field
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
         """Return a string representation of a ProgrammingLanguage object."""
-        return (f"{self.field}, {self.typing} Typing, Reflection={self.reflection}, "
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
                 f"First appeared in {self.year}")
 
     def is_dynamic(self):

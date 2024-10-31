b6f2cbb92940a14ec6025d482e8552f656139497
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-10-31 14:16:17 +1000

    Import ProgrammingLanguage class to prac 6 languages file

---
+++ prac_06/languages.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical - Client code to use the Car class.
+Estimate: 20 minutes
+Actual:   25 minutes
+"""
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)

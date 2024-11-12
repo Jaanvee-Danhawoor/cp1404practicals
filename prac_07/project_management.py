47f7bc3146460ab97a131e91f673a0419856c9f5
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-11-12 17:34:30 +1000

    Create load_projects() function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,17 +3,23 @@ Project Management Program
 Estimate: 3 hours
 Actual:
 """
-FILENAME = "projects.txt"
+from datetime import datetime
+from project import Project
+
+DEFAULT_FILENAME = "projects.txt"
 MENU = ("(L)oad projects\n(S)ave projects\n(D)isplay projects\n"
         "(F)ilter by date\n(A)dd new project\n(U)pdate project")
 
 
 def main():
+    projects = []
+    load_projects(projects, DEFAULT_FILENAME)
     print(MENU)
     choice = input("Enter your choice: ").upper()
     while choice != "Q":
         if choice == "L":
-            pass
+            filename = input("Enter filename: ")
+            load_projects(projects, filename)
         elif choice == "S":
             pass
         elif choice == "D":
@@ -28,6 +34,17 @@ def main():
             print("Invalid choice")
         print(MENU)
         choice = input("Enter your choice: ").upper()
+
+
+def load_projects(projects, filename):
+    """Load projects from file."""
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Ignore header
+        for line in in_file:
+            parts = line.strip().split("\t")
+            start_date = datetime.strptime(parts[1], "%d/%m/%Y").date()
+            project = Project(parts[0], start_date, int(parts[2]), float(parts[3]), float(parts[4]))
+            projects.append(project)
 
 
 main()

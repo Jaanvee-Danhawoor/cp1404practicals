4f721140f40af83339bc1d093b728b2e6542d553
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-11-12 15:46:42 +1000

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -10,9 +10,11 @@ def main():
     """Extract guitars from csv file and print guitars."""
     guitars = []
     extract_guitars_from_csv(FILENAME, guitars)
+    add_guitar(guitars)
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    add_guitars_to_csv(FILENAME, guitars)
 
 
 def extract_guitars_from_csv(filename, guitars):
@@ -22,6 +24,22 @@ def extract_guitars_from_csv(filename, guitars):
             parts = line.strip().split(',')
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
+
+
+def add_guitar(guitars):
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        name = input("Name: ")
+        guitars.append(guitar_to_add)
+
+
+def add_guitars_to_csv(filename, guitars):
+    with open(filename, "w", encoding="utf-8-sig") as out_file:
+        for guitar in guitars:
+            out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
 
 
 main()

420a783d13d22e5f8ee4fdba5ef0877650d26371
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-11-21 14:35:58 +1000

    Create SilverService class

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,21 @@
+from taxi import Taxi
+
+
+class SilverService(Taxi):
+    """Specialised version of a Taxi that includes fanciness."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.current_fare_distance = 0
+        self.price_per_km *= self.fanciness
+
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Get the current fare."""
+        return self.flagfall + super().get_fare()

f447904eeecc42d2c4bfcb45516d9c04e5fc43f3
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-11-21 14:36:24 +1000

    Test SilverService class

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,13 @@
+from prac_09.silver_service_taxi import SilverService
+
+
+def main():
+    """Test SilverService Taxi."""
+    silver_service_taxi = SilverService("Silver Taxi", 100, 2)
+    silver_service_taxi.drive(18)
+    print(silver_service_taxi)
+    print(f"For an 18 km trip in a SilverService taxi with fanciness 2, the fare should be $48.78 is $",
+          silver_service_taxi.get_fare())
+
+
+main()

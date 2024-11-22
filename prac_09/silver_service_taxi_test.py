db798e471be678e6d1c9cc39e8d512160c64f291
Jaanvee Danhawoor <jaanvee.danhawoor@my.jcu.edu.au>
2024-11-22 22:38:11 +1000

    Format fare to 2 decimal places

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,8 +6,8 @@ def main():
     silver_service_taxi = SilverService("Silver Taxi", 100, 2)
     silver_service_taxi.drive(18)
     print(silver_service_taxi)
-    print(f"For an 18 km trip in a SilverService taxi with fanciness 2, the fare should be $48.78 is $",
-          silver_service_taxi.get_fare())
+    print(f"For an 18 km trip in a SilverService taxi with fanciness 2, the fare should be $48.78 is "
+          f"${silver_service_taxi.get_fare():.2f}")
 
 
 main()
